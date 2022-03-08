f82786b397e27d4cd505bffe7ccedbdcb331be82
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-08 11:08:19 -0800

    Added proof-of-life of Auth0 authentication

--- src/App.js
+++ src/App.js
@@ -5,12 +5,31 @@ import Profile from './components/Profile';
 import AboutUs from './components/AboutUs';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react'
+import LogoutButton from './components/LogoutButton';
 
 class App extends React.Component {
+  printToken = async () => {
+    const res = await this.props.auth0.getIdTokenClaims();
+
+    const jwt = res.__raw;
+
+    console.log(jwt)
+  }
+
   render() {
     return (
       <>
         <Header />
+        {this.props.auth0.isAuthenticated ?
+          (
+          <>
+            <button onClick={this.printToken}>Button</button>
+            <LogoutButton />
+          </>
+          ) :
+          null
+        }
         <Router>
           <Routes>
             <Route path="/" element={<Body />} />
@@ -21,7 +40,6 @@ class App extends React.Component {
       </>
     );
   }
-
 }
 
-export default App;
+export default withAuth0(App);
