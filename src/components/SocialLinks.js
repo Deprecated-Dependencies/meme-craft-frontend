b41b187b42572690fc0963748ca2e8391a13f142
Micha Davis <m.leigh.davis@gmail.com>
2022-03-08 14:29:19 -0800

    add social links rendering to component

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import Image from 'react-bootstrap';
+import tumblr from '../img/social-tumblr.png';
+import reddit from '../img/social-reddit.png';
+import pintrest from '../img/social-pintrest.png';
+import instagram from '../img/social-ig.png';
+import facebook from '../img/social-fb.png';
 
 class SocialLinks extends React.Component {
   render() {
-    return <h3>Social Links</h3>;
+    let imgArray = [tumblr, reddit, pintrest, instagram, facebook]
+
+    return (
+      <>
+        {imgArray.map(img => <Image src={img} key={img} alt={`Share on social media`} />)}
+      </>
+    )
   }
 }
 
