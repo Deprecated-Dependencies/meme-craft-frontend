c4f448f41322ce46ebaa63e7e7dfee6168c05b2a
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-07 16:25:27 -0800

    Added cards and card grid

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,9 +1,36 @@
 import React from 'react';
-
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import deprecated from './deprecated.png'
 
 class ImageGallery extends React.Component {
   render() {
-    return <h3>Gallery</h3>;
+    let cardCount = 10;
+    let cards = [];
+    for (let i = 0; i < cardCount; i++){
+      cards.push(
+        (
+        <Col>  
+          <Card style={{width: "18rem"}}>
+            <Card.Img src={deprecated}/>
+            <Card.Body>
+              {i}
+            </Card.Body>
+          </Card>
+        </Col>
+        )
+      )
+    }
+
+    return (
+    <>
+      <h3>Gallery</h3>
+      <Container>
+        <Row xs={1} sm={2} md={3}> 
+            {cards}
+        </Row>
+      </Container>
+    </>
+    );
   }
 }
 
