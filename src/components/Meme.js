329f235fd262eb5d588e147fac783c2f16d44bd8
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-07 17:36:51 -0800

    Inserted MemeModal and added showModal state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 import deprecated from './deprecated.png'
+import MemeModal from './MemeModal';
 
 class Meme extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {showModal: false}
+  }
+
+  clickHandler = (event) => {
+    this.setState({showModal: true})
+  } 
+
+  handleModalClose = (event) => this.setState({showModal: false})
+
   render() {
     return (
     <Col>  
       <Card style={{width: "18rem"}}>
-        <Card.Img src={deprecated}/>
+        <Card.Img src={deprecated} onClick={this.clickHandler}/>
         <Card.Body>
           {this.props.memeData}
         </Card.Body>
+        <MemeModal 
+          show={this.state.showModal}
+          handleModalClose={this.handleModalClose}
+        />
       </Card>
     </Col>
     );
