ecac07f660cb954e82bc60864d8e611530f4746e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-08 11:06:49 -0800

    Added AuthProfile to Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import AuthProfile from './AuthProfile';
 
 class Profile extends React.Component {
   render() {
-    return <h3>Profile</h3>;
+    return (
+      <>
+        <h3>Profile</h3>
+        <AuthProfile />
+      </>
+      );
   }
 }
 
