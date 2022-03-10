a1797b27e56a012ab99bbebe6f634075b406defe
MichaelCampbell2020 <cam3000@gmail.com>
2022-03-09 17:43:35 -0800

    deleted col

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import MemeModal from './MemeModal';
 
 class Meme extends React.Component {
