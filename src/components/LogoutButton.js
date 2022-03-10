3b3adeef79ba19e17de13ce1798a8d715b49d40b
MichaelCampbell2020 <cam3000@gmail.com>
2022-03-09 16:50:22 -0800

    basic styling complete

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <button class = "btn btn-primary" onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
     </button>
   );
