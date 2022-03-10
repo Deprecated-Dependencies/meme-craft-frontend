4540e5cd8fdc1e2863a01c00cc1a87bc550cfd3a
Micha Davis <m.leigh.davis@gmail.com>
2022-03-10 11:47:39 -0800

    add header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Button, Container } from 'react-bootstrap';
 import Search from './Search';
 import Login from './Login';
 import LogoutButton from './LogoutButton'
-import {withAuth0} from '@auth0/auth0-react'
+import { withAuth0 } from '@auth0/auth0-react'
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-        <h3>Header</h3>
+        <h1 style={{ textAlign: "center" }}>MemeCraft</h1>
         <Image />
         {
-          this.props.auth0.isAuthenticated ? <LogoutButton/> :
-          <Login />
+          this.props.auth0.isAuthenticated ?
+            <div className="float-end" style={{ marginRight: "2%" }}>
+              <LogoutButton />
+              <a href="/profile" style={{ textDecoration: "none" }}><Button style={{ display: "block" }} className="btn-sm btn-secondary mr-1">My Profile</Button></a>
+            </div>
+            :
+            <Login />
         }
         <Search />
       </header>
